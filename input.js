add672934783d3763a3fa188191e4df04f7b3942
ruchir <ruchirjain452@gmail.com>
2021-11-01 18:55:29 +0530

    tree function implemented

--- input.js
+++ input.js
@@ -34,7 +34,41 @@ switch(command){
 
 function treeFn(dirPath){
     console.log("TREE command implemented for", dirPath);
+    let destPath;
+    if(dirPath==undefined){
+        console.log("kindly enter the path");
+        return;
+    }else{
+       let doesExist= fs.existsSync(dirPath);
+       if(doesExist){
 
+       treeHelper(dirPath, " ");
+
+       }
+       else{
+        console.log("kindly enter the path");
+        return;
+       }
+    }
+
+}
+
+function treeHelper(dirPath,indent){
+    //is file or folder
+    let isFile=fs.lstatSync(dirPath).isFile();
+    if(isFile==true){
+       let fileName= path.basename(dirPath);
+       console.log(indent + "__" + fileName);
+    }else{
+        let dirName=path.basename(dirPath);
+        console.log(indent+ "----" + dirName);
+        let childrens=fs.readdirSync(dirPath);
+
+        for(let i=0;i<childrens.length;i++){
+           let childPath= path.join( dirPath, childrens[i]);
+            treeHelper(childPath,indent + "\t");
+        }
+    }
 }
 
 function organizeFn(dirPath){
